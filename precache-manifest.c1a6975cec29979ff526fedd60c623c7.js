4df3468734cbffa29218631a7088f1c88f951bb1
timoxin.artur@gmail.com <timoxin.artur@gmail.com>
2019-04-01 21:27:55 +0300

    Updates

---
+++ precache-manifest.c1a6975cec29979ff526fedd60c623c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ae0009c03ed72639047",
+    "url": "/virtual-ecosystem/static/js/runtime~main.f83a892f.js"
+  },
+  {
+    "revision": "f8ff1ea5cafe7d657dd0",
+    "url": "/virtual-ecosystem/static/js/main.cb91f395.chunk.js"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/virtual-ecosystem/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "f8ff1ea5cafe7d657dd0",
+    "url": "/virtual-ecosystem/static/css/main.6df50b51.chunk.css"
+  },
+  {
+    "revision": "94da494a8e762e913dcfca77f63e3393",
+    "url": "/virtual-ecosystem/index.html"
+  }
+];
